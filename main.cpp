b8cb083d1104eda5b11610cba5e53c3aac7388e3
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-10-31 10:13:34 +0900

    双方向リストで駅名表示

--- main.cpp
+++ main.cpp
@@ -5,15 +5,65 @@
 #include <iostream>
 #include <windows.h> 
 
+class sta
+{
+public:
+
+};
+
 int main()
 {
-	std::list<char> station;
+	//1970年の山手線を定義
+	std::list<const char*> station =
+	{
+		"Tokyo","Yurakucho","Shimbashi","Hamamatsucho",
+		"Tamachi","Shinagawa","Osaki","Gotanda","Meguro",
+		"Shibuya","Harajuku","Yoyogi","Shinjuku",
+		"Shin-Okubo","Takadanobaba","Mejiro","Ikebukuro",
+		"Otsuka","Sugamo","Komagome","Nippori","Uguisudani",
+		"Ueno","Okachimachi","Akihabara","Kanda"
+	};
 
-	auto it = station.begin();
+	//1970年の駅名を表示
+	printf("1970年の山手線の駅を東京から内回りで表示\n");
+	for (std::list<const char*>::iterator it = station.begin(); it != station.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
 
-	it = "tokyo";
+	//日暮里の前に西日暮里を追加
+	for (std::list<const char*>::iterator it = station.begin(); it != station.end(); it++)
+	{
+		if (*it == "Nippori")
+		{
+			it = station.insert(it, "Nishi-Nippori");
+			it++;
+		}
+	}
 
-	std::cout << *it << std::endl;
+	//2019年の駅名を追加
+	printf("\n2019年の山手線の駅を東京から内回りで表示\n");
+	for (std::list<const char*>::iterator it = station.begin(); it != station.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	//品川の前に高輪ゲートウェイを追加
+	for (std::list<const char*>::iterator it = station.begin(); it != station.end(); it++)
+	{
+		if (*it == "Shinagawa")
+		{
+			it = station.insert(it, "Takanawa-Gateway");
+			it++;
+		}
+	}
+
+	//2022年の駅名を追加
+	printf("\n2022年の山手線の駅を東京から内回りで表示\n");
+	for (std::list<const char*>::iterator it = station.begin(); it != station.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
 
 	system("pause");
 
