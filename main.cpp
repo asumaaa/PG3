d69dec71c64a19621c0baff01b3cb9191d211134
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-11-30 12:08:50 +0900

    a

--- main.cpp
+++ main.cpp
@@ -4,147 +4,129 @@
 #include "list"
 #include <iostream>
 #include <windows.h> 
-
-//図書カード構造体
-struct Station
-{
-	int val;
-	char name[20];		//氏名
-	struct Station* prev;
-	struct Station* next;
-};
-
-void Create(Station* currentStation,const char *name);
-void Index(Station* Station);
-void IndexReturn(Station* Station);
-Station *GetInsertListAddress(Station* Station,int iterator);
+#include <algorithm> 
+#include<fstream>
+#include "sstream"
 
 int main()
 {
-	//駅名定義
-	const char* KeihinTohokuStation[] =
+	std::stringstream studentsList;
+	std::vector <std::string> students;
+	std::vector <int> num1;
+	std::vector <int> num2;
+
+	//各クラスの人数
+	int AClassVol = 24;
+	int BClassVol = 30;
+	int CClassVol = 25;
+	int DClassVol = 17;
+
+	//ファイルを開く
+	std::ifstream file;
+	file.open("student.csv");
+
+	//ファイルの内容をコピー
+	studentsList << file.rdbuf();
+
+	//ファイルを閉じる
+	file.close();
+
+	//1行分の文字列を入れる変数
+	std::string line;
+	int num = 1;
+	//生徒の名前と番号を代入
+	while (getline(studentsList, line, ','))
 	{
-		"Omiya","Saitama-Shintoshin","Yono","Kita-Urawa","Urawa","Minami-Urawa",
-		"Warabi","Nishi-Kawaguchi","Kawaguchi","Akabane","Higashi-Jujo","Oji",
-		"Kami-Nakazato","Tabata","Nishi-Nippori","Nippori","Uguisudani","Ueno",
-		"Okachimachi","Akihabara","Kanda","Tokyo","Yurakucho","Shimbashi",
-		"Hamamatsucho","Tamachi","Shinagawa","Oimachi","Omori","Kamata",
-		"Kawasaki","Tsurumi","Shin-Koyasu","Higashi-Kanagawa","Yokohama",
-	};
-
-	int iterator = 0;	
-	int stationVal = 35;
-	Station *station;
-
-	//先頭のアドレス
-	Station headStation;
-	headStation.next = nullptr;
-	headStation.prev = nullptr;
-
-	//高輪ゲートウェイ追加前の駅を代入
-	for (int i = 0; i < stationVal; i++)
-	{
-		station = GetInsertListAddress(&headStation, i);
-		Create(station, KeihinTohokuStation[i]);
+		//1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+		std::string word;
+		//カンマ区切りで先頭文字列を取得
+		getline(line_stream, word);
+		students.push_back(word);
+		num1.push_back(num);
+		num++;
 	}
 
-	//アドレスを戻す
-	station = GetInsertListAddress(&headStation, 0);
-	printf("高輪ゲートウェイ追加前の京浜東北線の駅を上りで表示\n");
-	//表示
-	Index(station);
-	printf("\n");
-
-	//アドレスを横浜に合わせる
-	station = GetInsertListAddress(&headStation, stationVal);
-	printf("高輪ゲートウェイ追加前の京浜東北線の駅を下りで表示\n");
-	//表示
-	IndexReturn(station);
-	printf("\n");
-
-	//高輪ゲートウェイ追加後
-	stationVal += 1;
-	//駅代入
-	for (int i = 0; i < stationVal; i++)
+	//名前によって番号代入
+	for (int i = 0; i < students.size(); i++)
 	{
-		station = GetInsertListAddress(&headStation, i);
-		if (i < 27)
-		{
-			Create(station, KeihinTohokuStation[i]);
+		std::string str1 = students[i].substr(0, 4);
+		std::string str2 = students[i].substr(5, 2);
+		if (str1 == "LE2A") {
+			num2.push_back(atoi(str2.c_str()));
 		}
-		else if (i == 27)
-		{
-			Create(station, "TakanawaGateway");
+		if (str1 == "LE2B") {
+			num2.push_back(AClassVol + atoi(str2.c_str()));
 		}
-		else
-		{
-			Create(station, KeihinTohokuStation[i - 1]);
+		if (str1 == "LE2C") {
+			num2.push_back(AClassVol + BClassVol + atoi(str2.c_str()));
+		}
+		if (str1 == "LE2D") {
+			num2.push_back(AClassVol + BClassVol + CClassVol + atoi(str2.c_str()));
 		}
 	}
 
-	//アドレスを戻す
-	station = GetInsertListAddress(&headStation, 0);
-	printf("高輪ゲートウェイ追加後の京浜東北線の駅を上りで表示\n");
-	//表示
-	Index(station);
+	/*std::vector<std::string> str = students;
+	for (int i = 0; i < students.size(); i++)
+	{
+		for (int j = 0; j < students.size(); j++)
+		{
+			if (num1[i] == num2[j])
+			{
+				std::replace(students.begin(), students.end(), str[i], students[j]);
+			}
+		}
+	}*/
+	std::sort(students.begin(), students.end());
+
+
+	//逐次実行
+	for_each(std::begin(students), std::end(students), [](std::string str) {std::cout << str << std::endl; });
+
+	/*for (int i = 0; i < students.size(); i++)
+	{
+		std::cout << students[i] << std::endl;
+	}*/
+
+	/*for (int i = 0; i < num1.size(); i++)
+	{
+		std::cout << num1[i] << std::endl;
+	}
+
 	printf("\n");
 
-	//アドレスを横浜に合わせる
-	station = GetInsertListAddress(&headStation, stationVal);
-	printf("高輪ゲートウェイ追加後の京浜東北線の駅を下りで表示\n");
-	//表示
-	IndexReturn(station);
+	for (int i = 0; i < num2.size(); i++)
+	{
+		std::cout << num2[i] << std::endl;
+	}*/
+
+	////各クラスの人数
+	//int AClassVol = 24;
+	//int BClassVol = 30;
+	//int CClassVol = 25;
+	//int DClassVol = 17;
+
+	//std::string str;
+	//int num;
+	//for (int i = 0; i < studentsList.size(); i++)
+	//{
+	//	//クラス名を取得
+	//	str = studentsList[i].substr(0.4);
+	//	if (str == "LE2A")num = AClassVol;
+	//	if (str == "LE2B")num = BClassVol;
+	//	if (str == "LE2C")num = CClassVol;
+	//	if (str == "LE2D")num = DClassVol;
+
+	//	//出席番号を取得
+	//	str = studentsList[i].substr(5.2);
+	//	num += atoi(str.c_str());
+
+	//	//置き換え
+	//	std::replace(students)
+	//}
+	
 
 	system("pause");
 	return 0;
 }
 
-void Create(Station* currentStation, const char* name)
-{
-	Station* newStation;
-	newStation = (Station*)malloc(sizeof(Station));
-
-	strcpy_s(newStation->name, 20, name);
-	newStation->prev = currentStation;
-	newStation->next = currentStation->next;
-
-	if (currentStation->next)
-	{
-		Station* nextStation = currentStation->next;
-		nextStation->prev = newStation;
-	}
-	currentStation->next = newStation;
-}
-
-void Index(Station* Station)
-{
-	while (Station->next != nullptr)
-	{
-		Station = Station->next;
-		printf("%s\n", Station->name);
-	}
-}
-void IndexReturn(Station* Station)
-{
-	while (Station->prev != nullptr)
-	{
-		printf("%s\n", Station->name);
-		Station = Station->prev;
-	}
-}
-
-Station *GetInsertListAddress(Station* Station, int iterator)
-{
-	for (int i = 0; i < iterator; i++)
-	{
-		if (Station->next)
-		{
-			Station = Station->next;
-		}
-		else
-		{
-			break;
-		}
-	}
-	return Station;
-}
